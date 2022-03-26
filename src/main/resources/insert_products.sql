fc4477671a3cef5b04521b6a174f2ad02bafdb26
ZITEC\diana.pascal <diana.pascal@zitec.com>
2022-03-26 13:32:52 +0200

    added inserts in products table

---
+++ src/main/resources/insert_products.sql
@@ -0,0 +1,39 @@
+insert into products(title, description, brand, volume,price,product_type,year,product_base_notes,image_url)
+values ('Good Girl', 'Apa de parfum Carolina Herrera Good Girl inspirată din conceptul dualității femeii moderne, reflectă contradicții și contraste pentru orice personalitate',
+        'Carolina Herrera', '150 ml', 449, 'parfum', 2020,
+        'ALCOHOL DENAT., PARFUM (FRAGRANCE), AQUA (WATER), LINALOOL, HYDROXYCITRONELLAL, BENZYL SALICYLATE, COUMARIN, ETHYLHEXYL METHOXYCINNAMATE, BENZYL BENZOATE',
+        'https://cdn.notinoimg.com/detail_zoom/carolina-herrera/8411061818961x_03/carolina-herrera-good-girl-eau-de-parfum-pentru-femei_.jpg');
+
+insert into products(title, description, brand, volume,price,product_type,year,product_base_notes,image_url)
+values ('Libre Intense', 'Nimic nu vă evidențiază spiritul liber și cuceritor mai bine ca apa de parfum pentru femei Yves Saint Laurent Libre Intense.',
+        'Yves Saint Laurent', '90 ml', 618, 'parfum', 2021,
+        'vetiver, ambra, vanilie Madagascar',
+        'https://cdn.notinoimg.com/detail_thumb_2k/yves-saint-laurent/3614273069540_04/yves-saint-laurent-libre-intense-eau-de-parfum-pentru-femei_.jpg');
+
+insert into products(title, description, brand, volume,price,product_type,year,product_base_notes,image_url)
+values ('Black Opium', 'Parfumul Yves Saint Laurent Black Opium te prinde în mrejele sale senzuale, de o feminitate inegalabilă.',
+        'Yves Saint Laurent', '50 ml', 317, 'parfum', 2014,
+        'vanilie, patchouli, cedru',
+        'https://cdn.notinoimg.com/detail_thumb_2k/yves-saint-laurent/yslblow_aedp20_02-o/yves-saint-laurent-black-opium-eau-de-parfum-pentru-femei___17.jpg');
+
+insert into products(title, description, brand, volume,price,product_type,year,product_base_notes,image_url)
+values ('La Vie Est Belle', 'Apa de parfum Lancôme La Vie Est Belle este simbolul libertății și al fericirii, o odă adusă tuturor frumuseților vieții. Aroma exclusiv feminină a fost creată pentru a le aduce tuturor zâmbetul pe buze.',
+        'Lancôme', '75 ml', 393, 'parfum', 2012,
+        'vanilie, patchouli, tonka, praline',
+        'https://cdn.notinoimg.com/detail_thumb_2k/lancome/3605532612836_01-o/lancome-la-vie-est-belle-eau-de-parfum-pentru-femei___30.jpg');
+
+insert into products(title, description, brand, volume,price,product_type,year,product_base_notes,image_url)
+values ('Artistique Iris Fétiche', 'Lejeră, proaspătă și inconfundabilă. Apa de parfum pentru femei Avon Artistique Iris Fétiche vă cucerește cu o compoziție fină și sofisticată, perfectă pentru fiecare zi.',
+        'Avon', '50 ml', 59, 'parfum', 2022,
+        'lemn de santal, vanilie, cihlimbar',
+        'https://cdn.notinoimg.com/detail_thumb_2k/avon/2800017989347_02-o/avon-artistique-iris-fetiche-eau-de-parfum-pentru-femei_.jpg');
+
+insert into products(title, description, brand, volume,price,product_type,year,product_base_notes,image_url)
+values ('Euphoria', 'Apa de parfum Calvin Klein Euphoria ne prezintă o lume misterioasă, plină de natură sălbatică și de flori exotice, care vă conferă un farmec senzual și o senzație de plăcere sublimată.',
+        'Calvin Klein', '100 ml', 184, 'parfum', 2005,
+        'violeta, mahon, cihlimbar, mosc',
+        'https://cdn.notinoimg.com/detail_zoom_2k/calvin-klein/cakephw_aedp10_03-o/calvin-klein-euphoria-eau-de-parfum-pentru-femei___32.jpg');
+
+
+
+
